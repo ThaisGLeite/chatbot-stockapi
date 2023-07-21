b7ecd2210714e8b9e3099b96c0bb5bde3ee0352a
ThaisGLeite <thaisdecarvalho@proton.me>
2023-07-21 13:50:18 -0300

    setup nats

--- src/main.go
+++ src/main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"log"
 	"net/http"
 	"strings"
+
+	"github.com/nats-io/nats.go"
 )
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
 
 func main() {
 	stock_code := "aapl.us"
@@ -17,8 +26,7 @@ func main() {
 
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Println("Error making the request:", err)
-		return
+		log.Fatal("Error making the request:", err)
 	}
 
 	defer resp.Body.Close()
@@ -27,14 +35,26 @@ func main() {
 
 	records, err := r.ReadAll()
 	if err != nil {
-		fmt.Println("Error reading CSV:", err)
-		return
+		log.Fatal("Error reading CSV:", err)
 	}
 
 	if len(records) < 2 || len(records[1]) < 7 {
-		fmt.Println("Unexpected data format")
-		return
+		log.Fatal("Unexpected data format")
 	}
 
-	fmt.Printf("%s quote is $%s per share\n", strings.ToUpper(stock_code), records[1][6])
+	quote := fmt.Sprintf("%s quote is $%s per share", strings.ToUpper(stock_code), records[1][6])
+
+	nc, err := nats.Connect(nats.DefaultURL)
+	failOnError(err, "Failed to connect to NATS")
+	defer nc.Close()
+
+	// Simple Publisher
+	err = nc.Publish("stock_quotes", []byte(quote))
+	failOnError(err, "Failed to publish a message")
+
+	// Flush the connection to the server, which ensures all messages
+	// have been processed by the server.
+	nc.Flush()
+
+	fmt.Println("Sent: ", quote)
 }
